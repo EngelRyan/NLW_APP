6bea6f0074560639634401803b162f82c7c0006b
EngelRyan <ryaneengel@gmail.com>
2024-09-10 21:16:51 -0300

    Array,Objeto and Function

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const message = "Hello World!!"
+//Array,Objeto and Function
+let metas = ["escovar","estudar"]
 
-{
-    const message = "eai meu"
-    console.log(message)
+console.log(metas[0] + " " + metas[1])
+
+let meta = {
+    value: 'Estudar 1h por dia',
+    checked: false,
+    isChecked: () => {}
 }
-console.log(message)
+console.log(meta.value)
+console.log(meta.checked)  
+console.log(meta.isChecked) 
+
+meta.checked  = true;
+
+console.log(meta.checked)
+
+
+// Arrow Function (PArece Lambda)
+const criarMeta = () => {}
+
+// Named Function
+function fazerMeta(){
+
+}
